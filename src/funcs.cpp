1cddbeea22664e871636a0ec5e2d12dea3374fa4
kperkins411 <kperkins411@gmal.com>
2021-08-12 14:20:05 -0400

    init

---
+++ src/funcs.cpp
@@ -0,0 +1,16 @@
+/*
+ * funcs.cpp
+ *
+ *  Created on: Aug 12, 2021
+ *      Author: keith
+ */
+
+#include "funcs.h"
+
+int get_int(){
+	return 42;
+}
+char get_char(){
+	return 'a';
+}
+
---
+++ src/funcs.h
@@ -0,0 +1,13 @@
+/*
+ * funcs.h
+ *
+ *  Created on: Aug 12, 2021
+ *      Author: keith
+ */
+
+#ifndef FUNCS_H_
+#define FUNCS_H_
+
+int get_int();
+char get_char();
+#endif /* FUNCS_H_ */
